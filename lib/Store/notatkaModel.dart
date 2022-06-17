82fa7cb9b73968c71f4108bd584dfefa9fadffe1
Adrian Malec <a.malec@ideo.pl>
2022-06-17 11:53:56 +0200

    Dodanie notatek do podstrony kalendarza

---
+++ lib/Store/notatkaModel.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import 'package:timehubmobile/Store/userModel.dart';
+
+class ModelNotatek extends ChangeNotifier {
+  List<Notatka> _notatki = [];
+  List<Notatka> notatkiZDnia = [];
+  bool _pobrano = false;
+  String bledy = "";
+  Future pobierz(BuildContext context, String miesiac, String rok) async {
+    _notatki = [];
+    final odpowiedz = await http.post(
+        Uri.parse('http://10.0.2.2:8000/api/uzytkownikNotatkiMiesiacRok/'),
+        headers: {
+          "Authorization": 'Bearer ' +
+              Provider.of<ModelUzytkownika>(context, listen: false).token,
+        },
+        body: {
+          'miesiac': miesiac.toString(),
+          'rok': rok.toString()
+        });
+    if (odpowiedz.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      _pobrano = true;
+      for (var notatka in jsonDecode(odpowiedz.body)) {
+        _notatki.add(Notatka.stworz(notatka['notatka']));
+      }
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      bledy = 'Taki użytkownik nie istnieje!';
+    }
+  }
+
+  Future filtrujNotatki(
+      BuildContext context, String miesiac, String rok, int dzien) async {
+    if (!_pobrano) {
+      await pobierz(context, miesiac, rok);
+    }
+    notatkiZDnia = [];
+    DateTime wybranyDzien = DateTime.parse(rok +
+        '-' +
+        (int.parse(miesiac) < 10 ? '0' + miesiac : miesiac) +
+        '-' +
+        (dzien < 10 ? '0' + dzien.toString() : dzien.toString()));
+    for (Notatka notatka in _notatki) {
+      if (wybranyDzien.day <= notatka.dataZakonczenia.day &&
+          wybranyDzien.day >= notatka.dataRozpoczecia.day) {
+        notatkiZDnia.add(notatka);
+      }
+    }
+  }
+}
+
+class Notatka {
+  int id;
+  int kolumna;
+  int stworzonePrzez;
+  DateTime dataStworzenia;
+  bool czyZrobione;
+  bool czyWazne;
+  String zawartosc;
+  DateTime dataRozpoczecia;
+  DateTime dataZakonczenia;
+
+  Notatka(
+      {required this.id,
+      required this.kolumna,
+      required this.stworzonePrzez,
+      required this.dataStworzenia,
+      required this.czyZrobione,
+      required this.czyWazne,
+      required this.zawartosc,
+      required this.dataRozpoczecia,
+      required this.dataZakonczenia});
+
+  factory Notatka.stworz(model) {
+    return Notatka(
+      id: model['id'],
+      kolumna: model['kolumna'],
+      stworzonePrzez: model['stworzone_przez'],
+      dataStworzenia: DateTime.parse(model['data_stworzenia']),
+      czyZrobione: model['czy_zrobione'],
+      czyWazne: model['czy_wazne'],
+      zawartosc: model['zawartosc'],
+      dataRozpoczecia: DateTime.parse(model['data_rozpoczecia']),
+      dataZakonczenia: DateTime.parse(model['data_zakonczenia']),
+    );
+  }
+}
--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timehubmobile/Store/notatkaModel.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   Calendar({Key? key}) : super(key: key);
 
-  final DateTime date = DateTime.now();
+  @override
+  State<Calendar> createState() => _stanKalendarza();
+}
 
+class _stanKalendarza extends State<Calendar> {
   String getMonthName(month) {
     switch (month) {
       case 1:
@@ -36,80 +41,198 @@ class Calendar extends StatelessWidget {
     }
   }
 
-  void hover(PointerEvent event) {
-    print(event);
-  }
-
+  int dzien = DateTime.now().day - 1;
+  int miesiac = DateTime.now().month;
+  int rok = DateTime.now().year;
   @override
   Widget build(BuildContext context) {
-    final DateTime lastDayOfMonth = new DateTime(date.year, date.month + 1, 0);
+    final DateTime lastDayOfMonth = new DateTime(rok, miesiac + 1, 0);
+    List<Notatka> notatki = [];
+    void pobierzNotatki() {
+      Provider.of<ModelNotatek>(context, listen: false).filtrujNotatki(
+          context, miesiac.toString(), rok.toString(), dzien + 1);
+      notatki = Provider.of<ModelNotatek>(context, listen: false).notatkiZDnia;
+    }
+
+    pobierzNotatki();
 
     return Scaffold(
-      body: Container(
-          padding:
-              const EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-          child: Column(
-            children: [
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blue),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    height: 300,
-                    width: MediaQuery.of(context).size.width - 80,
-                    child: Column(
-                      children: [
-                        Row(
+      appBar: AppBar(
+        title: const Text("Kalendarz"),
+      ),
+      body: Column(
+        children: [
+          Container(
+              padding: const EdgeInsets.only(
+                  top: 30, bottom: 20, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10))),
+                        height: 300,
+                        width: MediaQuery.of(context).size.width - 80,
+                        child: Column(
                           children: [
-                            Container(
-                                height: 40,
-                                padding: const EdgeInsets.only(bottom: 10),
-                                width: MediaQuery.of(context).size.width - 82,
-                                decoration: const BoxDecoration(
-                                    border: Border(
-                                        bottom:
-                                            BorderSide(color: Colors.blue))),
-                                child: Align(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                      getMonthName(date.month),
-                                      style: const TextStyle(fontSize: 30),
-                                    )))
-                          ],
-                        ),
-                        Center(
-                            child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: GridView.count(
-                            shrinkWrap: true,
-                            crossAxisCount: 7,
-                            children: List.generate(
-                                lastDayOfMonth.day,
-                                (int index) => Container(
-                                      color: index == date.day
-                                          ? Colors.blue
-                                          : Colors.transparent,
-                                      child: Center(
-                                        child: Text(
-                                          "$index",
-                                          style: TextStyle(
-                                            color: index == date.day
-                                                ? Colors.white
-                                                : Colors.black,
+                            Row(
+                              children: [
+                                Container(
+                                    height: 40,
+                                    width:
+                                        MediaQuery.of(context).size.width - 82,
+                                    decoration: const BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.blue))),
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          GestureDetector(
+                                            onTap: (() => setState(() {
+                                                  if (miesiac != 1) {
+                                                    miesiac--;
+                                                  } else {
+                                                    miesiac = 12;
+                                                    rok--;
+                                                  }
+                                                  Provider.of<ModelNotatek>(
+                                                          context,
+                                                          listen: false)
+                                                      .pobierz(
+                                                          context,
+                                                          miesiac.toString(),
+                                                          rok.toString());
+                                                  pobierzNotatki();
+                                                })),
+                                            child: const Icon(
+                                              Icons.arrow_left,
+                                              size: 35,
+                                            ),
                                           ),
-                                          textAlign: TextAlign.center,
-                                          overflow: TextOverflow.visible,
-                                        ),
-                                      ),
-                                    )),
-                          ),
+                                          Text(
+                                            getMonthName(miesiac) +
+                                                " " +
+                                                rok.toString(),
+                                            style:
+                                                const TextStyle(fontSize: 25),
+                                          ),
+                                          GestureDetector(
+                                            onTap: (() => setState(() {
+                                                  if (miesiac != 12) {
+                                                    miesiac++;
+                                                  } else {
+                                                    miesiac = 1;
+                                                    rok++;
+                                                  }
+                                                  Provider.of<ModelNotatek>(
+                                                          context,
+                                                          listen: false)
+                                                      .pobierz(
+                                                          context,
+                                                          miesiac.toString(),
+                                                          rok.toString());
+                                                  pobierzNotatki();
+                                                })),
+                                            child: const Icon(
+                                              Icons.arrow_right,
+                                              size: 35,
+                                            ),
+                                          ),
+                                        ]))
+                              ],
+                            ),
+                            Center(
+                                child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: GridView.count(
+                                shrinkWrap: true,
+                                crossAxisCount: 7,
+                                children: List.generate(
+                                    lastDayOfMonth.day,
+                                    (int index) => GestureDetector(
+                                        onTap: (() => setState(() {
+                                              dzien = index;
+                                            })),
+                                        child: Container(
+                                          color: index == dzien
+                                              ? Colors.blue
+                                              : Colors.transparent,
+                                          child: Center(
+                                            child: Text(
+                                              (index + 1).toString(),
+                                              style: TextStyle(
+                                                color: index == dzien
+                                                    ? Colors.white
+                                                    : Colors.black,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                              overflow: TextOverflow.visible,
+                                            ),
+                                          ),
+                                        ))),
+                              ),
+                            )),
+                          ],
                         )),
-                      ],
-                    )),
-              ]),
-            ],
-          )),
+                  ]),
+                ],
+              )),
+          ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+              physics: const ClampingScrollPhysics(),
+              children: [
+                Container(
+                    height: 260, // give it a fixed height constraint
+                    // child ListView
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.blue)),
+                    child: Column(children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width - 20,
+                          child: Center(
+                              child: Text((dzien + 1).toString() +
+                                  ' ' +
+                                  getMonthName(miesiac))),
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.blue)))),
+                      Expanded(
+                        child: notatki.isNotEmpty
+                            ? ListView.builder(
+                                itemCount: notatki.length,
+                                itemBuilder: (_, i) {
+                                  return Container(
+                                      margin: const EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.blue),
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10))),
+                                      child: ListTile(
+                                        leading: const Icon(Icons.note),
+                                        title: Text(notatki[i].zawartosc),
+                                        textColor: notatki[i].czyWazne
+                                            ? Colors.red
+                                            : Colors.black,
+                                      ));
+                                },
+                              )
+                            : const Center(
+                                child: Text("Brak zadań"),
+                              ),
+                      )
+                    ])),
+              ])
+        ],
+      ),
     );
   }
 }
